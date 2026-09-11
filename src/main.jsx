f5572072e985599f49cbbea8fbd0468cd32418f3
talaodeh04 <talaodeh388@gmail.com>
2026-09-11 13:09:19 +0300

    Fix GitHub Pages base path and SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/sneakdrop-testing">
         <ProductsProvider>
           <CartProvider>
             <WishlistProvider>
